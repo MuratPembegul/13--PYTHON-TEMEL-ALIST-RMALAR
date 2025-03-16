e4d10b5f8b8b6ef7e69f0615a21f9308674f47ee
Murat Pembegül <mpembegul15@gmail.com>
2025-03-16 13:13:07 +0100

    Add files via upload

---
+++ 13-) PYTHON TEMEL ALIŞTIRMALAR/9-) Kaç Basamaklı.py
@@ -0,0 +1,8 @@
+# Kullanıcıdan sayı al
+sayi = input("Bir sayı giriniz: ")
+
+# Sayının uzunluğunu hesapla
+basamak_sayisi = len(sayi)
+
+# Sonucu ekrana yazdır
+print(f"Girdiğiniz sayı {basamak_sayisi} basamaklıdır.")
